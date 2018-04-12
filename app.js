16ef5b619823ec109dde86991792dd86df042a2e
Cameron <cameron.grams.2@gmail.com>
2018-04-12 18:14:37 -0400

    ts heroku build

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require( 'body-parser' );
 const { mailer } = require( './mailer' ); 
 
 const app = express();
-const port = port.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
